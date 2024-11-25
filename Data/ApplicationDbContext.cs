b6eb9e024cc3f0532c20e3136eb50f48d1600710
hunor64 <nhunor06@gmail.com>
2024-11-25 12:59:07 +0100

    Cloned

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ForumMotor_13BC_H.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumMotor_13BC_H.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
